add439d3fea231a343949fd00e3e8048e236f703
johnstondevelop <johnstondevelop@gmail.com>
2026-05-22 13:10:02 -0400

    rock_paper_scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -2,25 +2,27 @@ import random;
 
 print("Welcome to Rock, Paper, and Scissors!")
 
-moves = ["rock", "paper", "scissors"]
+while True:
+    moves = ["rock", "paper", "scissors"]
 
-player_move = input("Enter your move! (rock, paper, scissors): ").lower()
+    player_move = input("Enter your move! (rock, paper, scissors): ").lower()
 
-if player_move not in moves:
-    print("Invalid Move! Please enter 'rock', 'paper', or 'scissors'.")
-else:
+    if player_move not in moves:
+        print("Invalid Move! Please enter 'rock', 'paper', or 'scissors'.")
+    else:
 
-    computer_move = random.choice(moves)
-print(f"Computer chose: {computer_move}")
-if player_move == computer_move:
+        computer_move = random.choice(moves)
+    print(f"Computer chose: {computer_move}")
+    if player_move == computer_move:
         print("It's a tie!")
-elif (player_move == "rock" and computer_move == "scissors") or (player_move == "scissors" and computer_move == "paper") or (player_move == "paper" and computer_move == "rock"):
-    print("You Win!")
-else:
-    print("You Lose!")
+    elif (player_move == "rock" and computer_move == "scissors") or (player_move == "scissors" and computer_move == "paper") or (player_move == "paper" and computer_move == "rock"):
+        print("You Win!")
+    else:
+        print("You Lose!")
 
-play_again = input("Do you want to play again? (yes or no):").lower()
-if play_again != "yes":
+    play_again = input("Do you want to play again? (yes or no):").lower()
+    if play_again != "yes":
         print("Thanks for playing!")
-else:
+    else:
         print("Then lets play again!")
+        break
