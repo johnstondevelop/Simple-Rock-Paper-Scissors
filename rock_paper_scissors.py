8a50e4a9e37d5061f2c1cdd8d4a7df5b486878f7
johnstondevelop <johnstondevelop@gmail.com>
2026-05-22 12:38:14 -0400

    rock-_paper_scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,24 @@
+import random;
+
+print("Welcome to Rock, Paper, and Scissors!")
+
+moves = ["rock", "paper", "scissors"]
+
+player_move = input("Enter your move! (rock, paper, scissors): ").lower()
+
+if player_move not in moves:
+    print("Invalid Move! Please enter 'rock', 'paper', or 'scissors'.")
+else:
+
+    computer_move = random.choice(moves)
+print(f"Computer chose: {computer_move}")
+if player_move == computer_move:
+        print("It's a tie!")
+elif (player_move == "rock" and computer_move == "scissors") or (player_move == "scissors" and computer_move == "paper") or (player_move == "paper" and computer_move == "rock"):
+    print("You Win!")
+
+print("You Lose!")
+
+play_again = input("Do you want to play again? (yes or no):").lower()
+if play_again != "yes":
+        print("Thanks for playing!")
